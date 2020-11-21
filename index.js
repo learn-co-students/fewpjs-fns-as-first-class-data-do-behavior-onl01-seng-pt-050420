885e1eeb723bce981090d5480b0d730eddc13838
Pwako11 <akolo@hotmail.com>
2020-11-21 10:14:25 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(timeString){
+
+  let spltTime = timeString.split(":");
+  let convTime = spltTime[0].concat(spltTime[1]);
+  let prsTime = parseInt(convTime, 10);
+
+  if (prsTime <1200){
+    return 'Good Morning';
+  }
+  else if ((prsTime >=1200) && (prsTime <= 1700)){
+    return 'Good Afternoon';
+  }
+  else if (prsTime > 1700){
+    return 'Good Evening';
+  }
+  else {
+    return 'TEST';
+  }
+}
+
+function displayMessage(input){
+
+  const greeting = document.getElementById("greeting");
+  greeting.textContent = input;
+  }
