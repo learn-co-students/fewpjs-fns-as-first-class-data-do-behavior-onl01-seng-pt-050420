df8e02395d87bd8c5bc67ec8967d2f71e08af7d6
RachelCodesToo <a.r.fausto@gmail.com>
2021-01-15 21:34:00 -0500

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+ function greet(timeString) {
+  const newTime = parseInt(timeString, 10);
+    
+  if (newTime < 12) return "Good Morning"
+  if (newTime > 17) return "Good Evening"
+  return "Good Afternoon"
+
+ }
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
